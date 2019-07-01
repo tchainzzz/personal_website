e6dd19edc27ae50e8e93d717967b9a5778f5c620
Trenton Chang <tchang97@myth62.stanford.edu>
2019-06-30 22:34:16 -0700

    Deleted old files

--- contactvalidator.js
+++
@@ -1,5 +0,0 @@
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name","req","Please provide your name");
-frmvalidator.addValidation("email","req","Please provide your email");
-frmvalidator.addValidation("email","email",
-  "Please enter a valid email address");
